034f2b40ae7c2ef41d3feaccce8126a0d67f17df
Darren Sim <klassiclegend@gmail.com>
2021-06-14 23:18:43 +0800

    Upload Button Example

---
+++ src/Components/ComponentEditor.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+</script>
+
+<main>
+  <div class="codepen-div">
+    <span>Live Example of Button Implementation</span>
+    <div
+      class="codepen"
+      data-height="500"
+      data-theme-id="light"
+      data-default-tab="js,result"
+      data-user="Mamboleoo"
+      data-slug-hash="XWJPxpZ"
+      style="height: 265px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;"
+      data-pen-title="Walkers - How to"
+    >
+      <span
+        >See the Pen <a href="https://codepen.io/Mamboleoo/pen/XWJPxpZ"> Walkers - How to</a> by Louis Hoebregts (<a
+          href="https://codepen.io/Mamboleoo">@Mamboleoo</a
+        >) on <a href="https://codepen.io">CodePen</a>.</span
+      >
+    </div>
+    <script async src="https://static.codepen.io/assets/embed/ei.js"></script>
+  </div>
+</main>
+
+<style>
+  .codepen-div {
+    width: 80vw;
+    font-weight: 700;
+    font-size: 24px;
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+  }
+</style>
--- src/Pages/Home.svelte
+++ src/Pages/Home.svelte
@@ -1,10 +1,26 @@
 <script lang="ts">
+  import ComponentEditor from '../Components/ComponentEditor.svelte';
   import PageHeader from '../Components/PageHeader.svelte';
 </script>
 
 <main>
   <PageHeader />
+  <div class="home-div">
+    <div class="welcome-text">Why bother with CSS Frameworks when you can copy paste the best practices from here!</div>
+    <ComponentEditor />
+  </div>
 </main>
 
 <style>
+  .home-div {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .welcome-text {
+    padding: 20px 0;
+    text-align: center;
+  }
 </style>
