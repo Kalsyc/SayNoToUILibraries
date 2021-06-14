79daaa2963a1d5ba1ca1c7a5979b96996dc92393
Darren Sim <klassiclegend@gmail.com>
2021-06-14 21:54:42 +0800

    Write in gitignore

--- src/Components/PageHeader.svelte
+++ src/Components/PageHeader.svelte
@@ -1,9 +1,24 @@
 <script lang="ts">
 </script>
 
-<div>
+<div class="wrapper">
   <div class="title-text">Say NO to UI Libraries!</div>
+  <div class="credits-div">
+    <span>Created by Darren Sim</span>
+  </div>
 </div>
 
 <style>
+  .wrapper {
+    height: 50px;
+    display: flex;
+    flex-direction: row;
+    background-color: beige;
+  }
+
+  .title-text {
+    font-family: Titillium Web;
+    font-weight: 700;
+    font-size: 24px;
+  }
 </style>
