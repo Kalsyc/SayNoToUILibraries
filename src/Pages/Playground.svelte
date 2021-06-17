9d11e4b8b67a528dda385bcc47de9e0dd121df0a
Darren Sim <klassiclegend@gmail.com>
2021-06-17 16:32:56 +0800

    Add in console

--- src/Pages/Playground.svelte
+++ src/Pages/Playground.svelte
@@ -1,6 +1,19 @@
 <script lang="ts">
   import Editor from '../Components/Editor.svelte';
 
+  const iframeLogger: string = `const _log = console.log;
+  console.log = function(...rest) {
+    window.parent.postMessage(
+      {
+        source: 'iframe',
+        message: rest,
+      },
+      '*'
+    );
+    _log.apply(console, arguments);
+  };
+`;
+
   let html: string = `<button class="general-btn fill-btn" onclick="onChange()">
   Press me!
 </button>
@@ -31,31 +44,42 @@
 
   let js: string = `const onChange = () => console.log("Pressed!")
 `;
+  let _js: string =
+    iframeLogger +
+    `const onChange = () => console.log("Pressed!")
+`;
 
-  $: srcDoc = `<html><body>${html}</body><style>${css}</style><scr` + `ipt>${js}</scr` + `ipt></html>`;
+  let logArray: string[] = [];
 
-  
-  const debounceHTML = (value) => {
-    const timer = setTimeout(() => {
-      html = value
+  $: srcDoc = `<html><body>${html}</body><sty` + `le>${css}</style><scr` + `ipt>${_js}</scr` + `ipt></html>`;
+
+  const debounceHTML = (value: string) => {
+    const timer: NodeJS.Timeout = setTimeout(() => {
+      html = value;
     }, 300);
     return () => clearTimeout(timer);
-  }
+  };
 
-  const debounceCSS = (value) => {
-    const timer = setTimeout(() => {
-      css = value
+  const debounceCSS = (value: string) => {
+    const timer: NodeJS.Timeout = setTimeout(() => {
+      css = value;
     }, 300);
     return () => clearTimeout(timer);
-  }
-  
-  const debounceJS = (value) => {
-    const timer = setTimeout(() => {
-      js = value
+  };
+
+  const debounceJS = (value: string) => {
+    const timer: NodeJS.Timeout = setTimeout(() => {
+      js = value;
+      _js = iframeLogger + value;
     }, 300);
     return () => clearTimeout(timer);
-  }
+  };
 
+  window.addEventListener('message', (res: MessageEvent<any>) => {
+    if (res.data && res.data.source === 'iframe') {
+      logArray = [...logArray, res.data.message[0]];
+    }
+  });
 </script>
 
 <main>
@@ -65,24 +89,43 @@
       <Editor lang="CSS" mode="css" onChange={debounceCSS} value={css} />
       <Editor lang="JS" mode="javascript" onChange={debounceJS} value={js} />
     </div>
-    <div class="divider"></div>
-    <div class="frame-panel">
-      <iframe contenteditable title="output" sandbox="allow-scripts" frameBorder="0" srcdoc={srcDoc} />
+    <div class="divider" />
+    <div class="frame-div">
+      <iframe
+        class="iframe-panel"
+        id="playground-frame"
+        contenteditable
+        title="output"
+        sandbox="allow-scripts"
+        frameBorder="0"
+        srcdoc={srcDoc}
+      />
+      <div class="console-panel">
+        {#each logArray as msg}
+          <span class="console-log">{msg}</span>
+          <div class="log-divider" />
+        {/each}
+      </div>
     </div>
   </div>
 </main>
 
 <style>
-
   .playground-wrapper {
     display: flex;
-    flex-direction: column; 
+    flex-direction: column;
   }
 
   .divider {
     border: black 2px solid;
     height: 0px;
     margin: 5px 0;
+  }
+
+  .log-divider {
+    border: black 0.5px solid;
+    height: 0px;
+    margin: 1px 0;
   }
 
   .editor-wrapper {
@@ -92,9 +135,27 @@
     background-color: aliceblue;
   }
 
-  .frame-panel {
-    height: 40vh;
+  .frame-div {
+    display: flex;
+    height: 80vh;
+    flex-direction: column;
     width: 100vw;
+  }
+
+  .iframe-panel {
+    height: 50%;
+  }
+
+  .console-panel {
+    height: 50%;
+    overflow-y: scroll;
+    background-color: #263238;
+    color: aliceblue;
+  }
+
+  .console-log {
+    overflow-wrap: anywhere;
+    font-size: 1em;
   }
 
   @media screen and (min-width: 768px) {
@@ -104,6 +165,16 @@
       flex-direction: row;
       justify-content: space-between;
       background-color: aliceblue;
+    }
+    .frame-div {
+      height: 40vh;
+      flex-direction: row;
+    }
+
+    .iframe-panel,
+    .console-panel {
+      height: 100%;
+      width: 50%;
     }
   }
 </style>
